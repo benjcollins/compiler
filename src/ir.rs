f3cf1a8483fe49da14015a9f967f02f748130c67
Ben Collins <benjcollins18@gmail.com>
2020-05-13 17:59:21 +0100

    made function return types behave correctly you can now call a function twice without overwriting the previous result

--- src/ir.rs
+++ src/ir.rs
@@ -2,12 +2,13 @@ use std::fmt;
 
 pub struct Program {
     functions: Vec<Function>,
-    variable_count: usize,
 }
 
 #[derive(Debug, Clone)]
 pub struct Function {
-    pub params: Vec<Var>,
+    params: Vec<Var>,
+    returns: Vec<Var>,
+    variable_count: usize,
     blocks: Vec<Block>,
     main: BlockId,
 }
@@ -46,6 +47,7 @@ pub enum Instruction {
     Call {
         function: FunctionId,
         args: Vec<Var>,
+        returns: Vec<Var>,
     },
     Return,
 }
@@ -58,14 +60,8 @@ pub struct Var {
 impl Program {
     pub fn new() -> Program {
         Program {
-            variable_count: 0,
             functions: vec![],
         }
-    }
-    fn new_variable(&mut self) -> Var {
-        let variable = Var { id: self.variable_count };
-        self.variable_count += 1;
-        variable
     }
     pub fn add_function(&mut self, function: Function) -> FunctionId {
         let id = self.functions.len();
@@ -76,15 +72,23 @@ impl Program {
 
 impl Function {
     pub fn new() -> Function {
-        Function { params: vec![], blocks: vec![], main: BlockId { id: 0 } }
+        Function { params: vec![], blocks: vec![], returns: vec![], variable_count: 0, main: BlockId { id: 0 } }
     }
     pub fn set_main(&mut self, block: BlockId) {
         self.main = block
     }
-    pub fn new_parameter(&mut self, program: &mut Program) -> Var {
-        let var = program.new_variable();
+    fn new_local_variable(&mut self) -> Var {
+        let variable = Var { id: self.variable_count };
+        self.variable_count += 1;
+        variable
+    }
+    pub fn new_parameter(&mut self) -> Var {
+        let var = self.new_local_variable();
         self.params.push(var);
         var
+    }
+    pub fn return_var(&mut self, var: Var) {
+        self.returns.push(var)
     }
     pub fn add_block(&mut self, block: Block) -> BlockId {
         let id = self.blocks.len();
@@ -97,23 +101,28 @@ impl Block {
     pub fn new() -> Block {
         Block { insts: vec![] }
     }
-    pub fn add_int(&mut self, a: Var, b: Var, program: &mut Program) -> Var {
-        let dest = program.new_variable();
+    pub fn add_int(&mut self, a: Var, b: Var, function: &mut Function) -> Var {
+        let dest = function.new_local_variable();
         self.insts.push(Instruction::AddInt { dest, a, b });
         dest
     }
-    pub fn constant_int(&mut self, constant: i32, program: &mut Program) -> Var {
-        let dest = program.new_variable();
+    pub fn constant_int(&mut self, constant: i32, function: &mut Function) -> Var {
+        let dest = function.new_local_variable();
         self.insts.push(Instruction::ConstantInt { dest, constant });
         dest
     }
-    pub fn phi(&mut self, a: Var, b: Var, program: &mut Program) -> Var {
-        let dest = program.new_variable();
+    pub fn phi(&mut self, a: Var, b: Var, function: &mut Function) -> Var {
+        let dest = function.new_local_variable();
         self.insts.push(Instruction::Phi { dest, a, b });
         dest
     }
-    pub fn call(&mut self, function: FunctionId, args: Vec<Var>) {
-        self.insts.push(Instruction::Call { function, args })
+    pub fn call(&mut self, target_function_id: FunctionId, args: Vec<Var>, return_count: usize, function: &mut Function) -> Vec<Var> {
+        let mut returns = Vec::new();
+        for _ in 0..return_count {
+            returns.push(function.new_local_variable())
+        }
+        self.insts.push(Instruction::Call { function: target_function_id, args, returns: returns.clone() });
+        returns
     }
     pub fn ret(&mut self) {
         self.insts.push(Instruction::Return)
@@ -131,7 +140,15 @@ impl fmt::Display for Program {
                     write!(f, ", r{}", param.id)?;
                 }
             }
-            writeln!(f, ")")?;
+            write!(f, ")")?;
+            let mut iter = function.returns.iter();
+            if let Some(ret) = iter.next() {
+                write!(f, " -> r{}", ret.id)?;
+                for ret in iter {
+                    write!(f, ", r{}", ret.id)?;
+                }
+            }
+            writeln!(f, "")?;
             for (block_id, block) in function.blocks.iter().enumerate() {
                 if block_id == function.main.id {
                     writeln!(f, "    main:")?;
@@ -153,7 +170,15 @@ impl fmt::Display for Program {
                         Instruction::Return => {
                             writeln!(f, "return")?
                         }
-                        Instruction::Call { function, args } => {
+                        Instruction::Call { function, args, returns } => {
+                            let mut iter = returns.iter();
+                            if let Some(var) = iter.next() {
+                                write!(f, "r{}", var.id)?;
+                                for var in iter {
+                                    write!(f, ", r{}", var.id)?;
+                                }
+                                write!(f, " = ")?;
+                            }
                             write!(f, "call {} (", function.id)?;
                             let mut iter = args.iter();
                             if let Some(var) = iter.next() {
--- src/types.rs
+++ src/types.rs
@@ -1,4 +1,4 @@
-use std::{rc::Rc, cell::RefCell};
+use std::{rc::Rc, cell::RefCell, slice::Iter};
 use crate::ir::{Program, Var, FunctionId, Block, Function};
 use crate::ast::{Parsed, Expr};
 
@@ -15,8 +15,8 @@ pub enum Type<'a, 'b> {
 
 #[derive(Debug)]
 pub struct Implementation<'a, 'b> {
-    pub params: Type<'a, 'b>,
-    pub returns: Type<'a, 'b>,
+    pub param_ty: Type<'a, 'b>,
+    pub return_ty: Type<'a, 'b>,
     pub function: FunctionId,
 }
 
@@ -32,14 +32,36 @@ impl<'a, 'b> PartialEq for Type<'a, 'b> {
 }
 
 impl<'a, 'b> Type<'a, 'b> {
-    pub fn merge(a: Type<'a, 'b>, b: Type<'a, 'b>, program: &mut Program, block: &mut Block) -> Option<Type<'a, 'b>> {
+    pub fn merge(a: Type<'a, 'b>, b: Type<'a, 'b>, function: &mut Function, block: &mut Block) -> Option<Type<'a, 'b>> {
         match (a, b) {
-            (Type::Int(a), Type::Int(b)) => Some(Type::Int(block.phi(a, b, program))),
+            (Type::Int(a), Type::Int(b)) => Some(Type::Int(block.phi(a, b, function))),
             (Type::Tuple(a), Type::Tuple(b)) if a.len() == b.len() => {
-                let merged: Option<Vec<Type<'a, 'b>>> = a.iter().zip(b).map(|(a, b)| Type::merge(a.clone(), b.clone(), program, block)).collect();
+                let merged: Option<Vec<Type<'a, 'b>>> = a.iter().zip(b).map(|(a, b)| Type::merge(a.clone(), b.clone(), function, block)).collect();
                 Some(Type::Tuple(merged?))
             }
             _ => None
+        }
+    }
+    pub fn return_type(&self, function: &mut Function) {
+        match self {
+            Type::Int(a) => function.return_var(*a),
+            Type::Tuple(types) => for ty in types {
+                ty.return_type(function)
+            }
+            Type::Func { .. } => (),
+        }
+    }
+    pub fn cast(&self, vars: &mut Iter<Var>) -> Type<'a, 'b> {
+        match self {
+            Type::Int(_) => Type::Int(*vars.next().unwrap()),
+            Type::Tuple(types) => {
+                let mut vec = vec![];
+                for ty in types {
+                    vec.push(ty.cast(vars))
+                }
+                Type::Tuple(vec)
+            }
+            Type::Func { .. } => self.clone(),
         }
     }
     pub fn vars_as_vec(&self) -> Vec<Var> {
@@ -55,13 +77,13 @@ impl<'a, 'b> Type<'a, 'b> {
             Type::Func { .. } => vec![],
         }
     }
-    pub fn as_paramater(&self, program: &mut Program, function: &mut Function) -> Type<'a, 'b> {
+    pub fn as_paramater(&self, function: &mut Function) -> Type<'a, 'b> {
         match self {
-            Type::Int(_) => Type::Int(function.new_parameter(program)),
+            Type::Int(_) => Type::Int(function.new_parameter()),
             Type::Tuple(types) => {
                 let mut vec = vec![];
                 for ty in types {
-                    vec.push(ty.as_paramater(program, function));
+                    vec.push(ty.as_paramater(function));
                 }
                 Type::Tuple(vec)
             }
