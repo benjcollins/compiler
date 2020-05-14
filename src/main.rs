3933a52163066556857dfb7d9d84aa706867b014
Ben Collins <benjcollins18@gmail.com>
2020-05-14 22:35:55 +0100

    updated main function to work with changes to the way blocks work

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,16 @@ use parser::parse_source;
 use compiler::compile;
 use scope::Scope;
 use std::fs;
-use ir::{Function, Program, Block};
+use ir::{Function, Program};
 
 fn main() {
     let source = fs::read_to_string("example.txt").unwrap();
     let ast = parse_source(&source).unwrap();
     let mut program = Program::new();
     let mut function = Function::new();
-    let mut block = Block::new();
+    let mut block = function.new_block();
     let _ = compile(&ast, &mut Scope::new(), &mut program, &mut function, &mut block).unwrap();
-    let block_id = function.add_block(block);
-    function.set_main(block_id);
+    function.submit_block(block);
     program.add_function(function);
     println!("{}", program);
 }
