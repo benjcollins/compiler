faf7871d5e34687ce3be5a16ce574a2866a05fdf
Ben Collins <benjcollins18@gmail.com>
2020-05-20 23:38:32 +0100

    equal types produce same register mapping

--- src/types.rs
+++ src/types.rs
@@ -6,8 +6,7 @@ use crate::ast::{Parsed, Expr};
 pub enum Type<'a, 'b> {
     Int(Var),
     Bool(Var),
-    Nothing,
-    Either(Var, Vec<Type<'a, 'b>>),
+    Maybe(Var, Box<Type<'a, 'b>>),
     Tuple(Vec<Type<'a, 'b>>),
     Func {
         pattern: &'b Parsed<'a, Expr<'a>>,
@@ -27,8 +26,15 @@ impl<'a, 'b> PartialEq for Type<'a, 'b> {
     fn eq(&self, other: &Type<'a, 'b>) -> bool {
         match (self, other) {
             (Type::Int(_), Type::Int(_)) => true,
-            (Type::Tuple(a), Type::Tuple(b)) if a.len() == b.len() => true,
-            (Type::Func { .. }, Type::Func { .. }) => true,
+            (Type::Bool(_), Type::Bool(_)) => true,
+            (Type::Tuple(atypes), Type::Tuple(btypes)) if atypes.len() == btypes.len() => {
+                for (a, b) in atypes.iter().zip(btypes) {
+                    if a != b {
+                        return false
+                    }
+                }
+                return true
+            }
             _ => false,
         }
     }
@@ -37,57 +43,70 @@ impl<'a, 'b> PartialEq for Type<'a, 'b> {
 impl<'a, 'b> Eq for Type<'a, 'b> {}
 
 impl<'a, 'b> Type<'a, 'b> {
-    pub fn merge(a: Type<'a, 'b>, b: Type<'a, 'b>, function: &mut Function, block: &mut Block) -> Option<Type<'a, 'b>> {
+    pub fn merge(a: &Type<'a, 'b>, b: &Type<'a, 'b>, function: &mut Function, block: &mut Block) -> Type<'a, 'b> {
         match (a, b) {
-            (Type::Int(a), Type::Int(b)) => Some(Type::Int(block.phi(a, b, function))),
-            (Type::Tuple(a), Type::Tuple(b)) if a.len() == b.len() => {
-                let merged: Option<Vec<Type<'a, 'b>>> = a.iter().zip(b).map(|(a, b)| Type::merge(a.clone(), b.clone(), function, block)).collect();
-                Some(Type::Tuple(merged?))
+            (Type::Int(a), Type::Int(b)) => Type::Int(block.phi(*a, *b, function)),
+            (Type::Bool(a), Type::Bool(b)) => Type::Bool(block.phi(*a, *b, function)),
+            (Type::Tuple(atypes), Type::Tuple(btypes)) if atypes.len() == btypes.len() => {
+                let mut types = vec![];
+                for (a, b) in atypes.iter().zip(btypes) {
+                    types.push(Type::merge(a, b, function, block))
+                }
+                Type::Tuple(types)
             }
-            _ => None
+            _ => unimplemented!(),
         }
     }
     pub fn get_used_vars(&self) -> Vec<Var> {
-        let mut vars = HashSet::new();
-        self.add_vars_to_map(&mut vars);
+        let mut vars = Vec::new();
+        self.add_vars_to_vec(&mut vars);
         vars.iter().cloned().collect::<Vec<Var>>()
     }
-    pub fn add_vars_to_map(&self, map: &mut HashSet<Var>) {
+    pub fn add_vars_to_vec(&self, map: &mut Vec<Var>) {
         match self {
-            Type::Int(var) => { map.insert(*var); },
-            Type::Bool(var) => { map.insert(*var); },
-            Type::Nothing => (),
-            Type::Either(var, types) => {
-                map.insert(*var);
-                for ty in types {
-                    ty.add_vars_to_map(map)
-                }
-            }
+            Type::Int(var) => { map.push(*var); },
+            Type::Bool(var) => { map.push(*var); },
+            Type::Maybe(var, ty) => {
+                map.push(*var);
+                ty.add_vars_to_vec(map);
+            },
             Type::Tuple(types) => for ty in types {
-                ty.add_vars_to_map(map)
+                ty.add_vars_to_vec(map)
             }
             Type::Func { .. } => (),
         }
     }
-    pub fn map_to(&self, map: &HashMap<Var, Var>) -> Type<'a, 'b> {
+    pub fn map_to(&self, mut vars: &[Var]) -> Type<'a, 'b> {
         match self {
-            Type::Int(var) => Type::Int(*map.get(var).unwrap()),
-            Type::Bool(var) => Type::Bool(*map.get(var).unwrap()),
-            Type::Nothing => Type::Nothing,
-            Type::Either(var, types) => {
-                Type::Either(*map.get(var).unwrap(), types.iter().map(|ty| ty.map_to(map)).collect())
-            }
-            Type::Tuple(types) => Type::Tuple(types.iter().map(|ty| ty.map_to(map)).collect()),
+            Type::Int(_) => Type::Int(vars[0]),
+            Type::Bool(_) => Type::Bool(vars[0]),
+            Type::Maybe(var, ty) => Type::Maybe(vars[0], Box::new(ty.map_to(&vars[1..]))),
+            Type::Tuple(types) => {
+                let mut vec = vec![];
+                for ty in types {
+                    vec.push(ty.map_to(&vars[..ty.size()]));
+                    vars = &vars[ty.size()..];
+                }
+                Type::Tuple(vec)
+            },
             Type::Func { .. } => self.clone(),
         }
     }
-    pub fn as_parameter_ty(&self, function: &mut Function) -> Type<'a, 'b> {
-        let vars = self.get_used_vars();
-        let mut map = HashMap::new();
-        for var in vars {
-            map.insert(var, function.new_parameter());
+    pub fn size(&self) -> usize {
+        match self {
+            Type::Int(_) => 1,
+            Type::Bool(_) => 1,
+            Type::Maybe(var, ty) => 1 + ty.size(),
+            Type::Tuple(types) => types.iter().map(|ty| ty.size()).sum(),
+            Type::Func { .. } => 0,
         }
-        self.map_to(&map)
+    }
+    pub fn as_parameter_ty(&self, function: &mut Function) -> Type<'a, 'b> {
+        let mut vars = vec![];
+        for _ in 0..self.size() {
+            vars.push(function.new_parameter());
+        }
+        self.map_to(&vars)
     }
     pub fn return_ty(&self, function: &mut Function) {
         for var in self.get_used_vars() {
