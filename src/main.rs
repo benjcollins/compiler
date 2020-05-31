e883a7cc18aaa5019f171faa216b94ae3c0e274a
benjcollins <38374664+benjcollins@users.noreply.github.com>
2020-05-31 12:04:41 +0100

    started using the vm to execute and test code

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,31 @@ mod ast;
 mod compiler;
 mod scope;
 mod types;
+mod execute;
 
-use parser::parse_source;
-use compiler::compile;
 use scope::Scope;
 use std::fs;
 use ir::{Function, Program};
+use execute::VirtualMachine;
 
 fn main() {
     let source = fs::read_to_string("example.txt").unwrap();
-    let ast = parse_source(&source).unwrap();
+    let ast = parser::parse_source(&source).unwrap();
     let mut program = Program::new();
     let mut function = Function::new();
     let mut block = function.new_block();
-    let ty = compile(&ast, &mut Scope::new(), &mut program, &mut function, &mut block).unwrap();
+    let ty = compiler::compile(&ast, &mut Scope::new(), &mut program, &mut function, &mut block).unwrap();
     block.ret(&mut function);
     ty.return_ty(&mut function);
-    program.add_function(function);
+    let main_id = program.add_function(function);
     println!("{}", program);
+
+    let function = program.get_function(main_id);
+
+    let mut vm = VirtualMachine::new(&program);
+    vm.execute(function);
+
+    for var in function.get_returns() {
+        println!("{}", vm.get_register(*var))
+    }
 }
